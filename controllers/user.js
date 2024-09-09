06fa6ab040d2f7465b632c43b3c77e22f0c412e4
Shail <shailjain2709@gmail.com>
2024-09-09 23:00:08 +0530

    Miscellaneous

--- controllers/user.js
+++ controllers/user.js
@@ -20,7 +20,7 @@ export const register = async (req, res) => {
 };
 
 // Verify login credentials
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
